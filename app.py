1a0a3251c0f523c8b3e2a3f0e7e02431bd1567e7
Ian Jones <ianj1993@gmail.com>
2025-11-05 12:54:52 -0700

    Added route for db_create

--- app.py
+++ app.py
@@ -11,3 +11,20 @@ def db_test():
     conn = psycopg2.connect("postgresql://flask_hello_world_crpk_user:kUVYi3G5SWBKIcqbhBBUjnIptSblXwIf@dpg-d45pj4ili9vc7385q5p0-a.oregon-postgres.render.com/flask_hello_world_crpk")
     conn.close()
     return 'Database connection test successful'
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect("postgresql://flask_hello_world_crpk_user:kUVYi3G5SWBKIcqbhBBUjnIptSblXwIf@dpg-d45pj4ili9vc7385q5p0-a.oregon-postgres.render.com/flask_hello_world_crpk")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+    ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball Table Successfully Created'
